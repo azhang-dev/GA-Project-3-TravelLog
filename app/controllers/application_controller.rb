750e96f07f12602a95d75705643417bd1dfd620a
Angela Zhang <a_zhang93@hotmail.com>
2022-03-14 21:56:55 +1100

    JWT setup application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 class ApplicationController < ActionController::Base
-    skip_before_action :verify_authenticity_token
-    include Knock::Authenticable
+    # skip_before_action :verify_authenticity_token
+    # include Knock::Authenticable
+    def secret_key
+        "SecretKey" # to ask Luke
+    end
+
+    def encode_token(payload)
+        JWT.encode(payload, secret_key, 'HS256')
+    end
+
+    def decode(token)
+        JWT.decode(token,"SecretKey", true, {algorithm: "HS256"})[0]
+    end
 end
