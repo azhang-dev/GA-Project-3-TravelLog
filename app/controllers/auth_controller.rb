65b76905b41a644998fab04d3eae01587cb9c705
Angela Zhang <a_zhang93@hotmail.com>
2022-03-15 12:41:53 +1100

    refactored login axiosrequest

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,28 +1,28 @@
 class AuthController < ApplicationController
-    skip_before_action :require_login, only: [:login, :auto_login]
+    # skip_before_action :require_login, only: [:login]
 
-    def login
-        user = User.find_by(username: params[:username])
-        if user && user.authenticate(params[:password])
-            payload = {user_id: user.id}
-            token = encode_token(payload)
-            render json: {user: user, jwt: token, sucess: "Welcome back, #{user.username}"}
-        else
-            render json: {failure: "Log in failed! Username or password invalid"}
-        end
-    end
+    # def login
+    #     user = User.find_by(email: params[:email])
+    #     if user && user.authenticate(params[:password])
+    #         payload = {user_id: user.id}
+    #         token = encode_token(payload)
+    #         render json: {user: user, jwt: token, sucess: "Welcome back, #{user.email}"}
+    #     else
+    #         render json: {failure: "Log in failed! Username or password invalid"}
+    #     end
+    # end
 
-    def auto_login
-        if session_user
-            render json: session_user
-        else
-            render json: {errors: "No User Logged In"}
-        end
-    end
+    # # def auto_login
+    # #     if session_user
+    # #         render json: session_user
+    # #     else
+    # #         render json: {errors: "No User Logged In"}
+    # #     end
+    # # end
 
-    def user_is_authed
-        render json: {message: "You are authorized"}
-    end
+    # def user_is_authed
+    #     render json: {message: "You are authorized"}
+    # end
     
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,10 @@ post "/users" => "users#create"
 #  resource :users, only: [:create]
  
  
-#  post "/login" => "auth#login"
 
-#  get "/auto_login" => "auth#auto_login"
 
-#  get "/user_is_authed" => "auth#user_is_authed"
+# get "/auto_login" => "auth#auto_login"
+
+
  
 end
