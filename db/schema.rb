3af1a4eeafced57a04cc18ddadc21f88ab228195
Angela Zhang <a_zhang93@hotmail.com>
2022-03-17 23:24:27 +1100

    migrated location for heroku

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_14_110538) do
+ActiveRecord::Schema.define(version: 2022_03_17_105431) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,20 @@ ActiveRecord::Schema.define(version: 2022_03_14_110538) do
 
   create_table "countries", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "locations", force: :cascade do |t|
+    t.string "name"
+    t.string "city"
+    t.string "country"
+    t.boolean "visited"
+    t.date "date_visited"
+    t.boolean "bucketlist"
+    t.text "images"
+    t.text "note"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
